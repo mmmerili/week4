45b272d56c41a27527d939ac540c311147306ce4
mmmerili <meriliru@metropolia.fi>
2021-04-14 09:25:23 +0300

    xd

--- wtpk-2021-week2-week3/app.js
+++ wtpk-2021-week2-week3/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const passport = require('./utils/pass');
 const rootRoute = require('./routes/rootRoute');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
@@ -10,6 +11,14 @@ const authRoute = require('./routes/authRoute');
 const app = express();
 const port = 3000;
 
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +26,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('week2_public_html'));
 app.use(express.static('uploads'));
 
+app.use(passport.initialize());
+app.use(passport.session());
 // routes
 // app.use('/', rootRoute);
 app.use('/auth', authRoute);
@@ -24,3 +35,14 @@ app.use('/cat',passport.authenticate('jwt', {session: false}), catRoute);
 app.use('/user',passport.authenticate('jwt', {session: false}), userRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
+    });
+
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
+});
